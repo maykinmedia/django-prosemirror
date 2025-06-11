4cac4a323a8f76d36b62154e357d9984f47565a8
Sidney Richards <sidney@maykinmedia.nl>
2025-06-11 11:25:57 +0200

    Add docstrings to the main modules

--- django_prosemirror/apps.py
+++ django_prosemirror/apps.py
@@ -1,5 +1,9 @@
+"""Django Prosemirror application configuration."""
+
 from django.apps import AppConfig
 
 
 class DjangoProsemirrorConfig(AppConfig):
+    """Configuration for the Django Prosemirror application."""
+
     name = "django_prosemirror"
