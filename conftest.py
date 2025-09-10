701022bce0595e7535b2fe1d6ac9ad51470c7714
Sidney Richards <sidney@maykinmedia.nl>
2025-09-10 08:47:19 +0200

    fixup! :building_construction: Only import filer logic if configured

--- conftest.py
+++ conftest.py
@@ -3,11 +3,11 @@ from django.test import override_settings
 import pytest
 
 
-@pytest.fixture(autouse=True, scope="session")
-def enable_filer_by_default():
-    """Enable filer image uploads by default for most tests."""
-    with override_settings(
-        DJANGO_PROSEMIRROR={"filer_image_uploads_enabled": True},
-        ROOT_URLCONF="testapp.urls",  # Force URL reload
-    ):
-        yield
+# @pytest.fixture(autouse=True, scope="session")
+# def enable_filer_by_default():
+#     """Enable filer image uploads by default for most tests."""
+#     with override_settings(
+#         DJANGO_PROSEMIRROR={"filer_image_uploads_enabled": True},
+#         ROOT_URLCONF="testapp.urls",  # Force URL reload
+#     ):
+#         yield
