5b24ffc53bac2fe7bc9d6e1cf43c52057abfc558
Sidney Richards <sidney@maykinmedia.nl>
2025-09-10 08:50:28 +0200

    fixup! fixup! :building_construction: Only import filer logic if configured

--- conftest.py
+++ conftest.py
@@ -3,11 +3,10 @@ from django.test import override_settings
 import pytest
 
 
-# @pytest.fixture(autouse=True, scope="session")
-# def enable_filer_by_default():
-#     """Enable filer image uploads by default for most tests."""
-#     with override_settings(
-#         DJANGO_PROSEMIRROR={"filer_image_uploads_enabled": True},
-#         ROOT_URLCONF="testapp.urls",  # Force URL reload
-#     ):
-#         yield
+@pytest.fixture(autouse=True, scope="session")
+def force_urlconf_reload():
+    """Force urlconf reload to process conditional URL patterns (e.g. for filer)."""
+    with override_settings(
+        ROOT_URLCONF="testapp.urls",  # Force URL reload
+    ):
+        yield
--- django_prosemirror/widgets.py
+++ django_prosemirror/widgets.py
@@ -51,9 +51,10 @@ class ProsemirrorWidget(Widget):
         attrs["classes"] = json.dumps(self.config.tag_to_classes)
         attrs["history"] = json.dumps(self.config.history)
         # Check if IMAGE node type is enabled and filer is available
-        has_image_support = NodeType.IMAGE in self.config.allowed_node_types
+        has_filer_image_support = NodeType.IMAGE in self.config.allowed_node_types
+        attrs["filer_upload_enabled"] = has_filer_image_support
         attrs["filer_upload_url"] = (
-            reverse("filer_upload_handler") if has_image_support else None
+            reverse("filer_upload_handler") if has_filer_image_support else None
         )
         return attrs
 
